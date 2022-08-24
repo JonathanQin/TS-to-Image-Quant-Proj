67700f9df0821067e22f9e5eedaec98e34aa9156
Jonathan Qin <jonathan.qin@gmail.com>
2022-08-24 16:24:29 -0700

    substantial edit to visualization, preprocessing

--- data_test.py
+++ data_test.py
@@ -4,64 +4,73 @@ Authors: Jonathan Qin
 Description: Pre-project test and experimentation with data processing
 """
 
+from time import time
 import numpy as np
 import pandas as pd
 import yfinance as yf
 import matplotlib.pyplot as plt
+from datetime import datetime
 
 #import os
 #import quantsc as Qsc
 
 def dataPreprocessing(data):
+    """_summary_
+    transpose, clean, normalize data
     
+    Args:
+        data (df): dataframe of time series
     """
-    Action: Transpose the data
-    Action: Clean Non-trading days
-    Action: Normalize data values to [-1:1]
-    """
-    data_T = data.transpose() #or data.T
-    print(data_T)
+    #removing weekends
+    #Convert DateTime column to datetime object
+    data = data[data['DateTime'].dt.weekday < 5].reset_index(drop=True)
     
-    start_date = data_T.iloc[0, 0] #iloc gives specific object, loc gives row/col
-    end_date = data_T.iloc[0, -1]
-    #print(start_date.shape)
-    #print(end_date.shape)
-    print(start_date)
-    print(end_date)
+    #remove after hours
+
+    #remove holidays
+        
+    #tranpose, columns become indices
+    data_T = data.transpose() #or data.T    
+    dataVisualization(data_T)
     
-    #dataVisualization(data_T)
+def gramianAngularField():
+    pass
     
+
     
 def dataVisualization(data):
+    """
+    display time series data
+
+    Args:
+        data (df): time series data to be displayed
+    """
     plt.figure()
     #plt.plot(data)
-    plt.title("AAPL Equity")
+    plt.title(data.index[1])
     plt.xlabel("DateTime")
     plt.ylabel("Price")
     
-    x = data.loc["DateTime", :]
-    y = data.loc["AAPL", :]
+    x = data.loc[data.index[0], :]
+    y = data.loc[data.index[1], :]
     
     plt.plot(x, y)
     plt.show()
     
 
-def gramianAngularField():
-    pass
-
 def main():
-    stock_list = ['AAPL', 'PFE', 'QQQ']
-    data = yf.download(stock_list, start = "2022-8-1", end = "2022-8-24", interval= "1h")
-    #print(data.size)
+    stock_list = ['SPY', 'PFE', 'AAPL']
+    data = yf.download(stock_list, start = "2012-8-16", end = "2022-8-20", interval= "1d")
     df = pd.DataFrame(data)
-    df_prices = df.loc[:, "Adj Close"]
+    df_prices = df.loc[:, "Adj Close"] #can also use df.drop(["unwanted columns"])
     #print(df.index) #gives a pd series of df index, not considered a column in tranposition
     df_prices = df_prices.rename_axis("DateTime").reset_index()
     #df_prices.rename({"index" : "DateTime"})   This renaming method is ineffective
     #print(df_prices.columns) #gives colummn names
-    df_AAPL = df_prices.loc[:, "DateTime" : "AAPL"]
-    #print(df_AAPL.shape, df_AAPL)
-    dataPreprocessing(df_AAPL)
+    df_SPY = df_prices.loc[:, ["DateTime", "SPY"]]
+
+    #print(df_SPY.shape, df_SPY)
+    dataPreprocessing(df_SPY)
 
 if __name__ == "__main__":
     main()
